3fbb3f3234be21ec64d9332bd313e9661bd3a7c6
Maninder T <manindersingh030@gmail.com>
2024-03-24 19:35:17 -0400

    Extracted music status

---
+++ app/src/main/java/com/taggar/dizi/models/MusicStatus.kt
@@ -0,0 +1,12 @@
+package com.taggar.dizi.models
+
+import android.app.PendingIntent
+import android.graphics.drawable.Icon
+
+data class MusicStatus(
+    val isPlaying: Boolean,
+    val actions: Map<String, PendingIntent>,
+    val icon: Icon,
+    val title: String,
+    val packageName: String
+)
--- app/src/main/java/com/taggar/dizi/services/NotificationListenerService.kt
+++ app/src/main/java/com/taggar/dizi/services/NotificationListenerService.kt
@@ -1,8 +1,10 @@
 package com.taggar.dizi.services
 
+import android.app.PendingIntent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import com.taggar.dizi.models.MusicStatus
 
 class NotificationListenerService : NotificationListenerService() {
     private val TAG = NotificationListenerService::class.java.simpleName
@@ -11,13 +13,31 @@ class NotificationListenerService : NotificationListenerService() {
     }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
-        // Called when a new notification is posted
         Log.d(TAG, "New notification posted: ${sbn.packageName} - ${sbn.notification?.tickerText}")
-        // You can access notification information such as package name, title, and content here
+        if (
+            sbn.notification.extras.getString("android.template")
+                ?.equals("android.app.Notification\$MediaStyle") == false ||
+            sbn.notification.actions.find { it.title.equals("Pause") || it.title.equals("Play") } == null
+        ) {
+            return
+        }
+        val actions = hashMapOf<String, PendingIntent>()
+        sbn.notification.actions.forEach {
+            val intent = it.actionIntent
+            val title = it.title.toString()
+            actions[title] = intent
+        }
+        val musicStatus = MusicStatus(
+            isPlaying = sbn.notification.actions.find { it.title.equals("Pause") } != null,
+            actions = actions,
+            icon = sbn.notification.getLargeIcon(),
+            title = sbn.notification.extras.getString("android.title") ?: "",
+            packageName = sbn.packageName
+        )
+        Log.d(TAG, "Found Music: $musicStatus")
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
-        // Called when a notification is removed
         Log.d(TAG, "Notification removed: ${sbn.packageName}")
     }
 }
